f9ae9b2fdcd2cdb7ba37818e6851aa399a8e71bf
ponmathi <ponmathittl@gmail.com>
2023-09-20 10:08:09 +0530

    implement products page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { Route } from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
+import { Route, Switch } from 'react-router-dom';
+import Welcome from './page/Welcome';
+import Products from './page/Products';
 import MainHeader from './Components/MainHeader';
+import ProductDetail from './page/ProductDetail';
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route Path="/welcome">
-          <Welcome />
-        </Route>
-        <Route Path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route Path="/welcome">
+            <Welcome />
+          </Route>
+          <Route Path="/products" exact>
+            <Products />
+          </Route>
+          <Route Path='/products/:productID/'>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
@@ -23,3 +29,4 @@ export default App;
 
 //our-domain.com/welcome => Welcome Component
 //our-domain.com/products => Products Component
+//our-domain.com/product-detail/a-book
